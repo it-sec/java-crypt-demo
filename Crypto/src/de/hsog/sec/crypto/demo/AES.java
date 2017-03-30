642c4c40db2a745dcc2f5b9a78fcd66c013471c5
Stephan Trahasch <stephan.trahasch@hs-offenburg.de>
2017-03-30 10:54:05 +0200

    Init

--- Crypto/src/de/hsog/sec/crypto/demo/AES.java
+++ Crypto/src/de/hsog/sec/crypto/demo/AES.java
@@ -16,31 +16,12 @@ public class AES {
 	private static String salt = "Offenburg";
 
 	public static String encrypt(String strToEncrypt, String secret) {
-		try {
-			// setKey(secret);
-			SecretKey myKey = getSecretKey(secret, salt, "AES", 256);
-
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, myKey);
-			
-			return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-		} catch (Exception e) {
-			System.out.println("Error while encrypting: " + e.toString());
-		}
+		// TODO
 		return null;
 	}
 
 	public static String decrypt(String strToDecrypt, String secret) {
-		try {
-			// setKey(secret);
-			SecretKey myKey = getSecretKey(secret, salt, "AES", 256);
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-			
-			cipher.init(Cipher.DECRYPT_MODE, myKey);
-			return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-		} catch (Exception e) {
-			System.out.println("Error while decrypting: " + e.toString());
-		}
+		// TODO
 		return null;
 	}
 
